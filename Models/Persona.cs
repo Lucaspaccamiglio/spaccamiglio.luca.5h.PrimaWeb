584a92777aec197881b698c429bbfead8bb9aff1
Lucaspaccamiglio <lucaspaccamiglio03@gmail.com>
2021-10-16 08:52:04 +0200

    Initial commit

---
+++ Models/Persona.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace spaccamiglio.luca._5h.PrimaWeb.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+    }
+}
